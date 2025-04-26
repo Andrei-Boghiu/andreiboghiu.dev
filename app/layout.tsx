83feacf69357663dd6c3824d8589c2e46029f284
Andrew <vandrei1408@yahoo.com>
2025-04-26 15:57:28 +0300

    feat: updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,9 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Andrei Boghiu | Software Solutions",
+  description:
+    "Helping businesses streamline workflows and grow through modern software solutions and expert technical consulting.",
 };
 
 export default function RootLayout({
@@ -22,12 +23,7 @@ export default function RootLayout({
         <link rel="icon" href="/jsm-logo.png" sizes="any" />
       </head>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
