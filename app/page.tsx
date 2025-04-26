b8b50d389ea48b17e7153783a6597a2554f81355
Andrew <vandrei1408@yahoo.com>
2025-04-26 17:06:50 +0300

    updated footer

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
-import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
@@ -18,7 +17,6 @@ const Home = () => {
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
-        <RecentProjects />
         <Clients />
         <Experience />
         <Approach />
